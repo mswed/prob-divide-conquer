472bfe18008ae88bf9f2ecee879a5ee90ccedc8f
mswed <mswed@beapot.com>
2023-12-11 00:30:35 -0500

    frequency finally works!

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -12,25 +12,16 @@ function sortedFrequency(arr, target) {
 }
 
 function findStartIndex(arr, target, finalIndex) {
-    console.log('Array is', arr)
 
     // find midpoint
     const midPoint = Math.floor(arr.length / 2)
     const midValue = arr[midPoint]
-    console.log('Received index is', finalIndex)
     finalIndex = finalIndex + midPoint
 
-    // search
-
-    console.log('Midpoint is', midPoint)
-    console.log('Final index is', finalIndex)
-    // console.log('Mid Value is', midValue)
     if (midValue === target) {
         // Found possible start
         if (arr[midPoint - 1] < target || arr[midPoint - 1] === undefined) {
             // Found start index
-            console.log('Start index is', midPoint)
-            console.log('Found start at', finalIndex)
             return finalIndex
         } else {
             const newArr = arr.slice(0, midPoint)
@@ -41,11 +32,8 @@ function findStartIndex(arr, target, finalIndex) {
 
     if (midValue < target) {
         if (arr.length === 1) {
-            console.log('START NOT FOUND')
             return -1
         } else {
-            console.log('Going right')
-            console.log(midPoint, arr.length)
             const newArr = arr.slice(midPoint, arr.length)
             return findStartIndex(newArr, target, finalIndex)
         }
@@ -53,13 +41,11 @@ function findStartIndex(arr, target, finalIndex) {
     }
 
     if (midValue > target) {
-        console.log('Going left')
         const newArr = arr.slice(0, midPoint)
         return findStartIndex(newArr, target, finalIndex - midPoint)
     }
 
     if (!arr.length || arr.length === 1) {
-        console.log('START NOT FOUND')
         return -1
     }
 
@@ -67,23 +53,15 @@ function findStartIndex(arr, target, finalIndex) {
 }
 
 function findEndIndex(arr, target, finalIndex) {
-    console.log('Array is', arr)
 
     // find midpoint
     const midPoint = Math.floor(arr.length / 2)
     const midValue = arr[midPoint]
-    console.log('Received index is', finalIndex)
     finalIndex = finalIndex + midPoint
-    console.log('Midpoint is', midPoint)
-    console.log('Final index is', finalIndex)
-    console.log(midValue, '===', target)
     if (midValue === target) {
-        console.log('Target matches mid value')
         // Found possible start
         if (arr[midPoint + 1] > target || arr[midPoint + 1] === undefined) {
             // Found end index
-            console.log('End index is', midPoint)
-            console.log('Found end at', finalIndex)
             return finalIndex
         } else {
             const newArr = arr.slice(midPoint, arr.length)
@@ -94,11 +72,8 @@ function findEndIndex(arr, target, finalIndex) {
 
     if (midValue < target) {
         if (arr.length === 1) {
-            console.log('END NOT FOUND')
             return -1
         } else {
-            console.log('Going right')
-            console.log(midPoint, arr.length)
             const newArr = arr.slice(midPoint, arr.length)
             return findEndIndex(newArr, target, finalIndex)
         }
@@ -106,13 +81,11 @@ function findEndIndex(arr, target, finalIndex) {
     }
 
     if (midValue > target) {
-        console.log('Going left')
         const newArr = arr.slice(0, midPoint)
         return findEndIndex(newArr, target, finalIndex - midPoint)
     }
 
     if (!arr.length || arr.length === 1) {
-        // console.log('END NOT FOUND')
         return -1
     }
 
