54ffb617caf9f6643b5f5c7c0173910f852274ae
mswed <mswed@beapot.com>
2023-12-11 10:34:08 -0500

    rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,33 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    // Find array rotation index
+    return rotationIndexSearch(arr)
 }
 
-module.exports = findRotationCount
+
+function rotationIndexSearch(arr) {
+    // Initialize search window
+    let start = 0
+    let end = arr.length - 1
+
+    while (start <= end) {
+        // Find midpoint
+        let mid = Math.floor((start + end) / 2)
+        if (arr[mid - 1] > arr[mid]) {
+            // We found the rotation index number
+            return mid
+        }
+
+        if (arr[mid] >= arr[end]) {
+            // Move window to the right
+            start = mid + 1
+        } else if (arr[mid] >= arr[start]) {
+            // Move window to the left
+            end = mid - 1
+        }
+    }
+
+    return 0
+
+}
+
+// module.exports = findRotationCount
